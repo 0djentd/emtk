55d1412445a0e4dd925719269df71e445bb1350b
0djentd <58089540+0djentd@users.noreply.github.com>
2021-12-14 17:32:41 +0300

    Clusters sorting update.

---
+++ classes/sorting_rule.py
@@ -0,0 +1,87 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+
+class SortingRule():
+    """
+    Base class for cluster sorting rules.
+    """
+
+    def __init__(self, s_name=None):
+        if s_name is None:
+            self.name = 'DefaultSortingRule'
+        else:
+            self.name = s_name
+
+        # Skip sanity checks for this rule
+        self.__SORTING_RULE_IS_SANE = False
+
+        # Names of cluster types this cluster should be placed after.
+        self.after_clusters = []
+
+        # Names of cluster types this cluster should be placed before.
+        self.before_clusters = []
+
+        # Sorting priority, used when some of the rules conflict
+        # with each other.
+        self.sorting_rule_priority = 0
+
+        # Should this cluster be placed last in the list?
+        self.last_cluster = False
+
+        # Should this cluster be placed first in the list?
+        self.first_cluster = False
+
+    @property
+    def name(self):
+        return self._sorting_rule_name
+
+    @name.setter
+    def name(self, s_name):
+        if isinstance(s_name, str):
+            self._sorting_rule_name = s_name
+        else:
+            raise TypeError
+
+    def check_sorting_rule_sanity(self):
+        """
+        Checks sorting rule for usual errors.
+        Return True or False.
+        """
+
+        if self._SortingRule__SORTING_RULE_IS_SANE:
+            return True
+
+        for x in self.after_clusters:
+            if not isinstance(x, str):
+                return False
+
+        for x in self.before_clusters:
+            if not isinstance(x, str):
+                return False
+
+        if not isinstance(self.sorting_rule_priority, int):
+            return False
+
+        if self.last_cluster and self.first_cluster:
+            return False
+
+        for x in self.after_clusters:
+            if x in self.before_clusters:
+                return False
+        return True
