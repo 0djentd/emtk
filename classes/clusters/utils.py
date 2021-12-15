d6f3da623093f5ef6b491344a2873c7151044ef2
0djentd <58089540+0djentd@users.noreply.github.com>
2021-12-15 18:01:02 +0300

    Modifiers clusters update 10.

---
+++ classes/clusters/utils.py
@@ -0,0 +1,47 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# import copy
+import json
+from .modifiers_cluster import ModifiersCluster
+
+
+def deserialize_cluster_type(serialized_cluster_type, **kwargs):
+    x = json.loads(serialized_cluster_type)
+    if x[0] == 'ModifiersCluster':
+        result = ModifiersCluster(
+                                  cluster_name=x[1],
+                                  cluster_type=x[2],
+                                  modifiers_by_type=x[3],
+                                  modifiers_by_name=x[4],
+                                  cluster_tags=x[5],
+                                  cluster_priority=x[6],
+                                  cluster_is_sane=x[7],
+                                  cluster_createable=x[8],
+                                  dont_define_cluster=False,
+                                  **kwargs
+                                  )
+    else:
+        raise TypeError
+    return result
+
+
+def serialize_cluster_type(cluster_type):
+    if not isinstance(cluster_type, ModifiersCluster):
+        raise TypeError
+    return cluster_type.serialize_this_cluster_type()
