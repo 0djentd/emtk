500cca445b6474981b70ee2224f81122796337c9
0djentd <58089540+0djentd@users.noreply.github.com>
2021-12-16 20:49:06 +0300

    Added some modifiers list tests.

--- simple_test_operator.py
+++ simple_test_operator.py
@@ -1,5 +1,25 @@
 #! python
 from lib.lists import ExtendedModifiersList
-x = ExtendedModifiersList()
+from lib.dummy_modifiers import DummyBlenderModifier
+from lib.dummy_modifiers import DummyBlenderObj
+import unittest
+
 print('ClustersList v1.0')
-print(f'{x}')
+
+
+class ModifiersListTests(unittest.TestCase):
+    def test_get_first(self):
+        mods = []
+        for x in range(5):
+            mods.append(DummyBlenderModifier)
+
+        o = DummyBlenderObj(mods)
+        a = ExtendedModifiersList()
+
+        a.create_modifiers_list(o)
+
+        self.assertEqual(a.get_first(), o.modifiers[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
