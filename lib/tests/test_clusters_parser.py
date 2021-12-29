3f96b4abf633ea793f00b2dc1cecb2ce3b8858aa
0djentd <58089540+0djentd@users.noreply.github.com>
2021-12-29 05:42:11 +0300

    Bugfix.

--- lib/tests/test_clusters_parser.py
+++ lib/tests/test_clusters_parser.py
@@ -18,32 +18,11 @@
 
 import unittest
 from lib.clusters.modifiers_cluster import ModifiersCluster
-from lib.clusters.clusters_layer import ClustersLayer
-from lib.utils.clusters import serialize_cluster_type, deserialize_cluster_type
-
-
-class ClustersSerializationTests():
-    def tearDown(self):
-        del(self.cluster)
-        del(self.serialized_cluster)
-
-    def test_serialize_deserialize(self):
-        cluster_2 = deserialize_cluster_type(self.serialized_cluster)
-        serialized_cluster_2 = serialize_cluster_type(cluster_2)
-        self.assertEqual(self.serialized_cluster, serialized_cluster_2)
-
-    def test_serialize_deserialize_progressive(self):
-        cluster_3 = deserialize_cluster_type(self.serialized_cluster)
-        serialized_cluster_3 = serialize_cluster_type(cluster_3)
-
-        cluster_2 = deserialize_cluster_type(serialized_cluster_3)
-        serialized_cluster_2 = serialize_cluster_type(cluster_2)
-
-        self.assertEqual(self.serialized_cluster, serialized_cluster_2)
 
 
 class ClustersSerializationClusterTests(
-        ClustersSerializationTests, unittest.TestCase):
+        unittest.TestCase):
+
     def setUp(self):
         self.cluster = ModifiersCluster(cluster_name='Beveled Boolean',
                                         cluster_type='BEVELED_BOOLEAN',
@@ -54,19 +33,12 @@ class ClustersSerializationClusterTests(
                                         cluster_createable=True,
                                         )
 
-        self.serialized_cluster = serialize_cluster_type(self.cluster)
+        self.serialized_cluster = self.cluster.get_this_cluster_definition()
 
+    def tearDown(self):
+        del(self.cluster)
+        del(self.serialized_cluster)
 
-class ClustersSerializationLayerTests(
-        ClustersSerializationTests, unittest.TestCase):
-
-    def setUp(self):
-        self.cluster = ClustersLayer(cluster_name='Double Bevel Layer',
-                                     cluster_type='BEVEL_CLUSTER',
-                                     modifiers_by_type=[
-                                         ['TRIPLE_BEVEL'], ['TRIPLE_BEVEL']],
-                                     modifiers_by_name=[['ANY'], ['ANY']],
-                                     cluster_priority=0,
-                                     cluster_createable=True,
-                                     )
-        self.serialized_cluster = serialize_cluster_type(self.cluster)
+    def test_get_cluster_definition(self):
+        x = self.cluster.get_this_cluster_definition()
+        self.assertEqual(x, self.serialized_cluster)
