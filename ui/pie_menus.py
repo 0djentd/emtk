1c335ff3449079c815f3ca0dcce9c373ad414a3a
0djentd <58089540+0djentd@users.noreply.github.com>
2022-01-18 01:40:46 +0300

    Removed unused classes.

--- ui/pie_menus.py
+++ ui/pie_menus.py
@@ -27,10 +27,7 @@ class VIEW3D_MT_PIE_bmtools_pie_1(Menu):
     def draw(self, context):
         layout = self.layout
         pie = layout.menu_pie()
-        # pie.operator("object.bmtoolm_2", text="BMToolM lite", icon="CUBE")
         pie.operator("object.bmtoolm", text="BMToolM", icon="CUBE")
         pie.operator("object.bmtoole2", text="Add modifier", icon="MOD_BEVEL")
         pie.operator("bmtools.clusters_list_popup",
                      text="Clusters list", icon="CUBE")
-        # pie.operator("bmtools.add_new_cluster",
-        #              text="Add new cluster", icon="CUBE")
