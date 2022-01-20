406a2ab3e4794d5d97a08ba3554d263b664a336c
0djentd <58089540+0djentd@users.noreply.github.com>
2022-01-20 03:35:34 +0300

    Removed unused class.

--- lib/lists/traits/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# from .dummy_modifiers_list import DummyModifiersListTrait
-# from modifiers_list_utils import ModifiersListUtilsTrait
-# 
-# __all__ = [
-#            DummyModifiersListTrait,
-#            ModifiersListUtilsTrait,
-#            ]
