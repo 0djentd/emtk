ab0ea15e98d59438f693597e0b2af9946afe541e
0djentd <58089540+0djentd@users.noreply.github.com>
2022-02-03 17:55:20 +0300

    Removed unused classes.

--- libs/emtk/clusters/__init__.py
+++ libs/emtk/clusters/__init__.py
@@ -22,4 +22,3 @@
 # from .modifiers_cluster import ModifiersCluster
 # from .default_modifier_cluster import DefaultModifierCluster 
 # from .clusters_layer import ClustersLayer
-# from .clusters_group import ClustersGroup
