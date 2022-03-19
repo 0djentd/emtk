787cf3360d7afbaddb711a435e3caaf106a22f07
0djentd <58089540+0djentd@users.noreply.github.com>
2022-03-19 14:50:57 +0300

    Revert "modified:   scripts/rebrand.sh"
    This reverts commit de84292ddbd9c57d8819d5b2630007b8adec8301.

--- scripts/rebrand.sh
+++ scripts/rebrand.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-fd -e py -X sed -i 's/EMTKL/libemtk/g'
-fd -e py -X sed -i 's/EMTK/libemtk/g'
-fd -e py -X sed -i 's/emtkl/libemtk/g'
-fd -e py -X sed -i 's/emtk/libemtk/g'
+fd -e py -X sed -i 's/EMTKL/libemtk'
+fd -e py -X sed -i 's/EMTK/libemtk'
+fd -e py -X sed -i 's/emtkl/libemtk'
+fd -e py -X sed -i 's/emtk/libemtk'
 
-fd -e py -X sed -i 's/BMTools/EMTK/g'
-fd -e py -X sed -i 's/BMTool/EMTK/g'
-fd -e py -X sed -i 's/bmtools/emtk/g'
-fd -e py -X sed -i 's/bmtool/emtk/g'
+fd -e py -X sed -i 's/BMTools/EMTK'
+fd -e py -X sed -i 's/BMTool/EMTK'
+fd -e py -X sed -i 's/bmtools/emtk'
+fd -e py -X sed -i 's/bmtool/emtk'
 
